cd19c5f32d7631f33a411920f628d7b39396e171
chillaranand <anand21nanda@gmail.com>
2017-01-13 12:29:36 +0530

    Add training samples

--- README.md
+++ README.md
@@ -10,7 +10,14 @@ This repo contains resources regarding Telugu OCR softwares.
 
 [banti_telugu_ocr](https://github.com/TeluguOCR/banti_telugu_ocr) - End to end OCR system for Telugu. Based on Convolutional Neural Networks.
 
+[Chamanti_ocr](https://github.com/rakeshvar/chamanti_ocr) - Telugu OCR framework using RNN, CTC in Theano & Python3.
+
+
 
 ## Files
 
-te.pyrnn.gz - Telugu Model trained with [ocropy](https://github.com/tmbdev/ocropy).
+generate_training_data.py - Generates training data using chamanti OCR
+
+te.pyrnn.gz - Telugu Model trained with [ocropy](https://github.com/tmbdev/ocropy)
+
+training_samples.zip - Few samples of training data
---
+++ generate_training_data.py
@@ -0,0 +1,58 @@
+"""
+Generates training data using chamanti OCR
+"""
+
+import os
+import sys
+
+from PIL import Image as im
+
+import telugu
+from scribe import Scribe
+
+
+def bin_arr_to_rgb_img(arr):
+    return bin_arr_to_img(arr).convert("RGB")
+
+
+def bin_arr_to_img(arr):
+    return im.fromarray((255 * (1 - arr)).astype("uint8"))
+
+
+options = {
+    'size': 24,
+    'maxangle': 0,
+    'dtype': 'float32',
+    'vbuffer': 0,
+    'noise': 0.05,
+    'hbuffer': 5,
+    'nchars_per_sample': 30,
+    'height': 45
+}
+alphabet_size = len(telugu.symbols)
+
+scriber = Scribe(language=telugu, **options)
+
+if len(sys.argv) > 1:
+    target_dir = sys.argv[1]
+    samples = int(sys.argv[2])
+else:
+    samples = 1000
+    target_dir = '../data/train/3/'
+
+print(samples, target_dir)
+
+try:
+    os.makedirs(target_dir)
+except FileExistsError:
+    pass
+
+for i in range(samples):
+    image, text, labels = scriber.get_text_image()
+    image = bin_arr_to_img(image)
+
+    base_name = '{}/{:04d}'.format(target_dir, i)
+    image.save(base_name + '.png')
+
+    with open(base_name + '.gt.txt', 'w') as fh:
+        fh.write(''.join(text))
